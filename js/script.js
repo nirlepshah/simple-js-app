47f508300b3ff60937d67953e41b83e7e3f6cccc
Nirlep Shah <nirlepshah@gmail.com>
2021-11-06 17:31:43 -0400

    Create Pokémon objects in script.js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let pokemonList = [
+  {
+   name: 'Caterpie',
+   height: 2,
+   type: 'bug' ,
+   weight: 3   
+  }, 
+  
+  {
+      name: 'Charmeleon',
+      height: 1,
+      type: 'bug' ,
+      weight: 19   
+  }, 
+  
+  {
+      name: 'Parasect',
+      height: 1,
+      type: ['grass', 'bug'] ,
+      weight: 3   
+  },
+
+  {
+      name: 'Sneasel',
+      height: 3,
+      type: ['dark', 'ice'] ,
+      weight: 28   
+  }
+
+];
+

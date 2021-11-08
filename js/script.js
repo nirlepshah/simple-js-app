1c3f43d3dc2276641c12bed443d054c1357024bf
Nirlep Shah <nirlepshah@gmail.com>
2021-11-08 17:40:38 -0500

    Add for loop that iterates over each item in the PokemonList

--- js/script.js
+++ js/script.js
@@ -29,3 +29,33 @@ let pokemonList = [
 
 ];
 
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(
+      "<p>" + (i + 1) + ": " + pokemonList[i].name + ": Pokémon name" + "</p>"
+    );
+  
+    if (pokemonList[i].height > 2) {
+      document.write(
+        "<p>" +
+          '"' +
+          pokemonList[i].name +
+          " (height:" +
+          pokemonList[i].height +
+          ") - Wow, that's big\"" +
+          "<p>" +
+          "-----"
+      );
+    } else {
+      document.write(
+        "<p>" +
+          '"' +
+          " " +
+          pokemonList[i].name +
+          " (height:" +
+          pokemonList[i].height +
+          ')"' +
+          "<p>" +
+          "-----"
+      );
+    }
+  }
